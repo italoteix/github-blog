f825867a4893d0f80f9231950adb9b9c6d71e512
Italo Teixeira <italoteixeira@SouthPoles-MacBook-Air.local>
2023-02-05 23:52:51 +0100

    chore: set src alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [react(), svgr()],
 })
